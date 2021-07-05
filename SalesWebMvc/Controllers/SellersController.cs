778422d645e2dda74b8a145c4be24cbc3ae84037
Mateus Mourao <mateusvmourao@gmail.com>
2021-07-05 15:11:23 -0300

    Tratamento de exceção para

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -69,8 +69,15 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Delete(int id)
         {
-            await _sellerService.RemoveAsync(id);
-            return RedirectToAction(nameof(Index));
+            try
+            {
+                await _sellerService.RemoveAsync(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch ()
+            {
+
+            }
         }
 
         public async Task<IActionResult> Details(int? id)
